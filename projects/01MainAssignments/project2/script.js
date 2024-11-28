2604d4fd149afe946f198857c30c93c0d0a47859
Aline Wyss <aline.wyss@zhdk.ch>
2024-11-28 09:55:50 +0100

    change of the font

--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -4,7 +4,7 @@ let font;
 const messages = ["Na los!", "Klick mich!", "Hier bin ich!", "Siehst du mich nicht?", "Zu langsam!","Ich schlafe gleich ein!"];
 
 function preload() {
-  font = loadFont("/fonts/Arial-Rounded-Bold.otf");
+  font = loadFont("/fonts/AmericanTypewriter.otf");
 }
 
 function setup() {
@@ -14,8 +14,8 @@ function setup() {
 
 function createNewButton() {
   let newButton = createButton(random(messages)); 
-  newButton.size(random(80, 120), random(40, 60));
-  newButton.position(random(width - 50), random(height - 20));
+  newButton.size(random(100, 160), random(70, 100));
+  newButton.position(random(width - 50), random(height- 20));
   newButton.mouseOver(() => moveAndSpawn(newButton));
   buttons.push(newButton);
 }
